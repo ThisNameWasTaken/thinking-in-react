3e76da613c6731eb575854ebbe346fee30ac558e
Adrian <adriandinca2008@yahoo.com>
2018-12-05 13:40:12 +0200

    refactor: Replace App with FilterableTable

---
+++ src/FilterableTable.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './FilterableTable.scss';
+
+class FilterableTable extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default FilterableTable;
--- src/FilterableTable.test.js
+++ src/FilterableTable.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import FilterableTable from './FilterableTable';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<FilterableTable />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
